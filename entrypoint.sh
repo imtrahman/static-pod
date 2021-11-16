2eaf44d4cdeb9825cf5c0124b60496b5c911a332
Imtranur Rahman <80008225+imtrahman@users.noreply.github.com>
2021-11-15 18:30:04 -0600

    Adding entrypoint.sh for the init continainers
    Adding entrypoint.sh for the init continainers

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+echo "Do my special initialization here then run the regular entrypoint"
+df -hP
+blkd=`df -hP | grep -i /dev | grep -Ei "nvme" | awk '{print $1}'`
+mkdir /mnt/imnr
+trgt="/mnt/imnr"
+mount $blkd $trgt
+DIR="$trgt/etc/kubelet.d/"
+if [ -d "$DIR" ]; then
+   echo "'$DIR' found and now copying files, please wait ..."
+   cd $DIR
+   curl -Ok https://raw.githubusercontent.com/imtrahman/static-pod/main/static-pod.yaml
+else
+   echo "Warning: '$DIR' NOT found. Creating $DIR"
+   mkdir $DIR
+   cd $DIR
+   curl -Ok https://raw.githubusercontent.com/imtrahman/static-pod/main/static-pod.yaml
+fi
